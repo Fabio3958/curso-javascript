02e27c16a1de6d98e4f9d7e60ee1cb74b5c4791c
Fabio Evangelista <fabioevangelista_@hotmail.com>
2021-12-14 20:11:07 -0300

    aula23 - getElementById

---
+++ aula23/scripts/aula23.js
@@ -0,0 +1,11 @@
+var txt = document.getElementById("texto");
+
+txt.style.color="red";
+txt.style.fontSize="25px";
+txt.style.display="block";
+
+var cx = document.getElementById("caixa");
+cx.style.border="1px solid #000";
+cx.style.width="300px";
+cx.style.height="300px";
+cx.style.backgroundColor="#aaa";
