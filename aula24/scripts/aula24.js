2287f79dd44ee40258afef3d72409cfeb89f6b4f
Fabio Evangelista <fabioevangelista_@hotmail.com>
2021-12-15 19:04:34 -0300

    aula24 - innerHTML

---
+++ aula24/scripts/aula24.js
@@ -0,0 +1,7 @@
+var cxt = document.getElementById("cxtexto");
+var txt = document.getElementById("texto");
+
+cxt.value="Curso de Javascript";
+txt = txt.innerHTML="Aula 24 - innerHTML"; //com atribuicao, modifica
+txt = txt.innerHTML; //sem atribuicao, só captura
+
