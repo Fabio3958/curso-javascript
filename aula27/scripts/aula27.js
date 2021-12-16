3d84d4dc5476b954c0e9f4a6f9cc311f2e0883a8
Fabio Evangelista <fabioevangelista_@hotmail.com>
2021-12-16 20:01:10 -0300

    aula27 - Manipulando Forms com Javascript

---
+++ aula27/scripts/aula27.js
@@ -0,0 +1,15 @@
+//var fm = document.forms[1].id;
+//var fm = document.forms.item(0).id;
+//var fm = document.forms.namedItem("curso2").innerHTML;
+document.write(document.getElementById("curso1").elements[0].value);
+
+var fm = document.forms[0];
+var el = fm.elements;
+
+for (var i = 0; i < el.length - 1; i++) {
+
+    el[i].style.backgroundColor = "#ddd";
+    el[i].value = "CFBCursos";
+}
+
+document.write(fm);
